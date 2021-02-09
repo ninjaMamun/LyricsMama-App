7f853acbb5901f699013a704d5f2c8fbf3377b6a
Mamun <shahriyar.m.mamun@gmail.com>
2021-02-09 20:52:29 +0600

    final touch

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LyricsMama-App
+## Live Site Link:
+
+https://ninjamamun.github.io/LyricsMama-App/
--- app.js
+++ app.js
@@ -15,16 +15,6 @@ const displaySongs = songs =>{
         <div class="col-md-9">
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
-            <figure>
-                <figcaption>Song Preview:</figcaption>
-                <br>
-                <audio
-                    controls
-                    src="${song.preview}">
-                        Your browser does not support the
-                        <code>audio</code> element.
-                </audio>
-            </figure>
 
         </div>
         <div class="col-md-3 text-md-right text-center">
